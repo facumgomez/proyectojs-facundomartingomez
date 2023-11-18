d4b7854e937d6bd336f620e716dfbf3695f9d402
facumgomez <gomezmfacundo@gmail.com>
2023-11-18 14:03:06 -0300

    segunda pre entrega se agrego filter, objetos y trate de comprimir codigo

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
+function iniciarCompra (){
+
 const productos = ["COMBO DE MANCUERNAS 25kg", "BARRA DOMINADAS PARED", "BAND DOMINADAS", "RODILLERA DEPORTIVA", "SOGA PARA SALTAR"];
-const precio = [75000, 50000, 15000, 12000, 10000]
-const formaPago = ["Efectivo (10% de descuento)", "Tarjeta de debito (en 1 pago)", "3 cuotas sin interes", "6 cuotas fijas (recargo del 10%)", "12 cuotas fijas (recargo del 20%)"]
+const precio = [75000, 50000, 15000, 12000, 10000];
+const formaPago = ["Efectivo (10% de descuento)", "Tarjeta de debito (en 1 pago)", "3 cuotas sin interes", "6 cuotas fijas (recargo del 10%)", "12 cuotas fijas (recargo del 20%)"];
 let selecProd = "Seleccione producto que desea comprar:\n";
 let selecPago = "Seleccione medio de pago que desea:\n";
 let cantidad = "Ingrese la cantidad que desea: ";
@@ -13,9 +15,8 @@ let cuotas = 0;
 let precioFinal = 0;
 let ingresoVal = false;
 
-
-const msjPantalla = function (msj, list) {
-    let j = 0;
+const mostrarMsj = function (msj, list) {
+    let j = " ";
 
     for (let i = 0; i < list.length; i++) {
         j = i + 1;
@@ -28,21 +29,22 @@ const msjPantalla = function (msj, list) {
     return msj;
 }
 
+function filtrarProductos(palabraClave) {
+    return productos.filter(producto => producto.toLowerCase().includes(palabraClave.toLowerCase()));
+}
 function validacion(mensaje, lista) {
     let seleccionado;
 
     while (true) {
-        seleccionado = parseInt(prompt(msjPantalla(mensaje, lista)));
+        seleccionado = parseInt(prompt(mostrarMsj(mensaje, lista)));
 
         if (!isNaN(seleccionado) && seleccionado != null && seleccionado != "") {
             break;
-        }
-        else {
-            alert("Por favor, ingrese una opción valida");
-            continue;
-        }
-
+        } else {
+        alert("Por favor, ingrese una opción válida");
+        continue;
     }
+}
     return seleccionado;
 }
 
@@ -54,16 +56,15 @@ function cantidadProducto(mensaje) {
 
         if (!isNaN(cantidad) && cantidad != null && cantidad != "") {
             break;
-        }
-        else {
-            alert("Por favor, ingrese una cantidad valida");
+        } else {
+            alert("Por favor, ingrese una cantidad válida");
             continue;
         }
     }
     return cantidad;
 }
 
-function calprecioFinal(precio, cant, descuento, cuotas) {
+function calPrecioFinal(precio, cant, descuento, cuotas) {
     let precioFinal = precio * cant;
     let precioDescuento = 0;
     let recargo = 0;
@@ -73,17 +74,17 @@ function calprecioFinal(precio, cant, descuento, cuotas) {
         precioFinal = precioFinal - precioDescuento;
     }
 
-    if (cuotas == 3) {
+    if (cuotas === 3) {
         precioFinal = precioFinal / cuotas;
     }
 
-    if (cuotas == 6) {
+    if (cuotas === 6) {
         recargo = ((precioFinal * 10) / 100);
         precioFinal = precioFinal + recargo;
         precioFinal = precioFinal / cuotas;
     }
 
-    if (cuotas == 12) {
+    if (cuotas === 12) {
         recargo = ((precioFinal * 20) / 100);
         precioFinal = precioFinal + recargo;
         precioFinal = precioFinal / cuotas;
@@ -91,120 +92,56 @@ function calprecioFinal(precio, cant, descuento, cuotas) {
     return precioFinal;
 }
 
-function detalleProducto(productoSe) {
-    let opcionesPago = "";
+function detalleProducto(productoSeleccionado) {
+    const palabraClave = prompt("Ingrese una palabra clave para filtrar productos:");
+    const productosFiltrados = filtrarProductos(palabraClave);
 
-    /*Solicito que ingrese cantidad de producto */
+    if (productosFiltrados.length > 0) {
+    prodSelec = validacion(selecProd, productosFiltrados);
+    ingresoVal = true;}else {
+        alert("No hay productos disponibles que coincidan con la búsqueda.");
+    }
+
+
     cantidadProd = cantidadProducto(cantidad);
-
-    opcionesPago = "Producto seleccionado: " + productos[productoSe - 1] + "\n";
+    let opcionesPago = "";
+    opcionesPago = "Producto seleccionado: " + productos[productoSeleccionado - 1] + "\n";
     opcionesPago = opcionesPago + "Cantidad: " + cantidadProd + "\n";
-    opcionesPago = opcionesPago + "Valor por unidad: " + "$" + precio[productoSe - 1] + "\n";
-    opcionesPago = opcionesPago + "Total: " + "$" + precio[productoSe - 1] * cantidadProd + "\n\n";
+    opcionesPago = opcionesPago + "Valor por unidad: " + "$" + precio[productoSeleccionado - 1] + "\n";
+    opcionesPago = opcionesPago + "Total: " + "$" + precio[productoSeleccionado - 1] * cantidadProd + "\n\n";
     opcionesPago = opcionesPago + selecPago;
     formadePago = validacion(opcionesPago, formaPago);
-}
+} 
 
-prodSelec = validacion(selecProd, productos);
+    prodSelec = validacion(selecProd, productos);
 
-switch (prodSelec) {
-    case 1:
+    if (prodSelec >= 1 && prodSelec <= 5) {
         ingresoVal = true;
         detalleProducto(prodSelec);
-        break;
-
-    case 2:
-        ingresoVal = true;
-        detalleProducto(prodSelec);
-        break;
-
-    case 3:
-        ingresoVal = true;
-        detalleProducto(prodSelec);
-        break;
-
-    case 4:
-        ingresoVal = true;
-        detalleProducto(prodSelec);
-        break;
-
-    case 5:
-        ingresoVal = true;
-        detalleProducto(prodSelec);
-        break;
-
-    case 6:
-        alert("¡Nos vemos hasta su proxima compra!");
-        break;
-
-    default:
+    } else if (prodSelec === 6) {
+        alert("¡Nos vemos en su próxima compra!");
+    } else {
         alert("La opción que desea ingresar no se encuentra disponible");
-        break;
-}
+    }
 
-if (ingresoVal) {
-    switch (formadePago) {
-        case 1:
-            descuento = 10;
-            cuotas = 0;
-            precioFinal = calprecioFinal(precio[prodSelec - 1], cantidadProd, descuento, cuotas)
-            detallePago = detallePago + "Producto: " + productos[prodSelec - 1] + "\n";
-            detallePago = detallePago + "Cantidad: " + cantidadProd + "\n";
-            detallePago = detallePago + "Forma de pago: " + formaPago[formadePago - 1] + "\n";
-            detallePago = detallePago + "Total a pagar: $" + precioFinal;
-            alert(detallePago);
-            break;
-
-        case 2:
-            descuento = 0;
-            cuotas = 0;
-            precioFinal = calprecioFinal(precio[prodSelec - 1], cantidadProd, descuento, cuotas)
-            detallePago = detallePago + "Producto: " + productos[prodSelec - 1] + "\n";
-            detallePago = detallePago + "Cantidad: " + cantidadProd + "\n";
-            detallePago = detallePago + "Forma de pago: " + formaPago[formadePago - 1] + "\n";
-            detallePago = detallePago + "Total a pagar: $" + precioFinal;
-            alert(detallePago);
-            break;
-
-        case 3:
-            descuento = 0;
-            cuotas = 3;
-            precioFinal = calprecioFinal(precio[prodSelec - 1], cantidadProd, descuento, cuotas)
-            detallePago = detallePago + "Producto: " + productos[prodSelec - 1] + "\n";
-            detallePago = detallePago + "Cantidad: " + cantidadProd + "\n";
-            detallePago = detallePago + "Forma de pago: " + formaPago[formadePago - 1] + "\n";
-            detallePago = detallePago + "Total a pagar: " + "3 cuotas de $" + precioFinal.toFixed(2);
-            alert(detallePago);
-            break;
-
-        case 4:
-            descuento = 0;
-            cuotas = 6;
-            precioFinal = calprecioFinal(precio[prodSelec - 1], cantidadProd, descuento, cuotas)
-            detallePago = detallePago + "Producto: " + productos[prodSelec - 1] + "\n";
-            detallePago = detallePago + "Cantidad: " + cantidadProd + "\n";
-            detallePago = detallePago + "Forma de pago: " + formaPago[formadePago - 1] + "\n";
-            detallePago = detallePago + "Total a pagar: " + "6 cuotas de $" + precioFinal.toFixed(2);
-            alert(detallePago);
-            break;
-
-        case 5:
-            descuento = 0;
-            cuotas = 12;
-            precioFinal = calprecioFinal(precio[prodSelec - 1], cantidadProd, descuento, cuotas)
-            detallePago = detallePago + "Producto: " + productos[prodSelec - 1] + "\n";
-            detallePago = detallePago + "Cantidad: " + cantidadProd + "\n";
-            detallePago = detallePago + "Forma de pago: " + formaPago[formadePago - 1] + "\n";
-            detallePago = detallePago + "Total a pagar: " + "12 cuotas de $" + precioFinal.toFixed(2);
-            alert(detallePago);
-            break;
-
-        case 6:
-            alert("¡Nos vemos hasta su proxima compra!");
-            break;
-    
-        default:
-            alert("La opción que desea ingresar no se encuentra disponible");
-            break;
+    if (ingresoVal) {
+        if (formadePago >= 1 && formadePago <= 5) {
+        descuento = (formadePago === 1) ? 10 : 0;
+        cuotas = (formadePago >= 3 && formadePago <= 5) ? (formadePago - 1) * 3 : 0;
+        precioFinal = calPrecioFinal(precio[prodSelec - 1], cantidadProd, descuento, cuotas);
+        detallePago = detallePago + "Producto: " + productos[prodSelec - 1] + "\n";
+        detallePago = detallePago + "Cantidad: " + cantidadProd + "\n";
+        detallePago = detallePago + "Forma de pago: " + formaPago[formadePago - 1] + "\n";
+        if (cuotas > 0) {
+            detallePago = detallePago + "Total a pagar: " + cuotas + " cuotas de $" + precioFinal.toFixed(2);
+        } else {
+            detallePago = detallePago + "Total a pagar: $" + precioFinal.toFixed(2);
+        }
+        alert(detallePago);
+        } else if (formadePago === 6) {
+        alert("¡Nos vemos en su próxima compra!");
+        } else {
+        alert("La opción que desea ingresar no se encuentra disponible");
+        }
     }
 }
